70fb96bcc270f9c63ee2afb10dda0a5da80cc645
Yasas Senarath <ysenarath.93@gmail.com>
2021-06-30 20:31:14 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r) as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as fh:
+    requirements = fh.read().strip().split('\n')
+
+setuptools.setup(
+    name='tranquil',
+    version='0.0.1',
+    author='Yasas Senarath',
+    description='Twitter Academic API Client.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ysenarath/tranquil',
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
